3898c638c60509c16428ab9b8c1263ac477670c6
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2024-12-15 20:10:12 +0600

    Update sum-of-1-to-n.cpp

--- data-structure/time-complexity/sum-of-1-to-n.cpp
+++ data-structure/time-complexity/sum-of-1-to-n.cpp
@@ -6,6 +6,12 @@ int main()
     int n;
     cin >> n;
 
+    // using loop
+    // int sum = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     sum += i;
+    // }
 
     // time complexity
     int sum = (n * (n + 1)) / 2;
