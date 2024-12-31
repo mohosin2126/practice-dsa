2ec3f40c9ed08e7841d28edc4eb37e99f13f2e50
mohosin2126 <mohosin2126@gmail.com>
2024-12-31 15:21:08 +0600

    added another example of logarithmic complexity

--- data-structure/time-complexity/logarithmic-complexity.cpp
+++ data-structure/time-complexity/logarithmic-complexity.cpp
@@ -4,10 +4,23 @@ int main()
 {
     // O(logN)
     // i*=2 or i/=2 (logarithmic complexity)
+
+    //-----No One ------//
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i*=2){
-        cout<<i<<endl;
+    cin >> n;
+    for (int i = 1; i <= n; i *= 2)
+    {
+        cout << i << endl;
+    }
+
+    //-----No Two ------//
+
+    int k;
+    cin >> k;
+    for (int i = 1; i < n; i++)
+    {
+        cout << i << endl;
+        i = i * k;
     }
     return 0;
 }
