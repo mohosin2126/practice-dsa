166be6d8fc4b12c410c0c3812981220f3594047c
mohosin2126 <mohosin2126@gmail.com>
2024-12-31 15:14:32 +0600

    linear complexity done

---
+++ data-structure/time-complexity/linear-complexity.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // linear complexity O(N)
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
